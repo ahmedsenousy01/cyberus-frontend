4a67a6d8358c39e349ece4b0d7472801e1defcea
AhmedSenousy <ahmedsenousy01@gmail.com>
2022-10-29 09:08:11 +0200

    setup the project, installed needed packages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar.component'
+import AddPhoto from './Pages/AddPhoto/AddPhoto'
+import Home from './Pages/Home/Home'
+import NotFound404 from './Pages/NotFound404/NotFound404'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return (<>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/add-photo' element={<AddPhoto />} />
+      <Route path='*' element={<NotFound404 />} />
+    </Routes>
+  </>)
 }
 
-export default App;
+export default App
